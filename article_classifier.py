b4a2b66c7847a8e973a097ad6b3f3b16c01864f7
anapysasi <57515498+anapysasi@users.noreply.github.com>
2021-03-13 22:08:04 -0600

    Update article_classifier.py

--- article_classifier.py
+++ article_classifier.py
@@ -13,6 +13,7 @@ import cleaning_article
 import matching_articles
 import requests
 from sklearn.model_selection import train_test_split
+from multiprocessing import Pool, TimeoutError
 import time
 
 def get_dictionary(cleaned_tokens_list):
@@ -24,55 +25,66 @@ def get_dictionary(cleaned_tokens_list):
     for tokens in cleaned_tokens_list:
         yield dict([token, True] for token in tokens)
 
-start = time.time()
-# Compiling articles for dataset
-left_articles_text, right_articles_text = matching_articles.compile_politics(text=True)
+if __name__ == '__main__':
+    start = time.time()
+    # Compiling articles for dataset
+    left_articles_text, right_articles_text = matching_articles.compile_politics(text=True)
 
-left_cleaned_tokens_list = cleaning_article.run(left_articles_text)
-right_cleaned_tokens_list = cleaning_article.run(right_articles_text)
+    with Pool(processes=4) as pool:
+        left_cleaned_tokens_list = pool.map(cleaning_article.run, left_articles_text)
+        right_cleaned_tokens_list = pool.map(cleaning_article.run, right_articles_text)
+    end = time.time()
 
-left_tokens_for_model = list(get_dictionary(left_cleaned_tokens_list))
-right_tokens_for_model = list(get_dictionary(right_cleaned_tokens_list))
+    # left_cleaned_tokens_list = cleaning_article.run(left_articles_text)
+    # right_cleaned_tokens_list = cleaning_article.run(right_articles_text)
 
-# Tagging with Left and Right
-left_dataset = [(left_dict, "Left")
-                for left_dict in left_tokens_for_model]
+    left_tokens_for_model = list(get_dictionary(left_cleaned_tokens_list))
+    right_tokens_for_model = list(get_dictionary(right_cleaned_tokens_list))
 
-right_dataset = [(right_dict, "Right")
-                 for right_dict in right_tokens_for_model]
+    # Tagging with Left and Right
+    left_dataset = [(left_dict, "Left")
+                    for left_dict in left_tokens_for_model]
 
-dataset = left_dataset + right_dataset
+    right_dataset = [(right_dict, "Right")
+                     for right_dict in right_tokens_for_model]
 
-# Making train and test dataset split, 70/30
-Train, Test = train_test_split(dataset, random_state=6741, test_size=0.3)
+    dataset = left_dataset + right_dataset
 
-# Build classifier
-classifier = NaiveBayesClassifier.train(Train)
+    # Making train and test dataset split, 70/30
+    Train, Test = train_test_split(dataset, random_state=6741, test_size=0.3)
 
-# Test Validation
-print("Accuracy is:", classify.accuracy(classifier, Test))
+    # Build classifier
+    classifier = NaiveBayesClassifier.train(Train)
 
-print(classifier.show_most_informative_features(20))
-end = time.time()
-print("Runtime:", end - start)
+    # Test Validation
+    print("Accuracy is:", classify.accuracy(classifier, Test))
 
-# Pass in url of article to classify
-url_test = input('Copy an Article url Here: ')
+    print(classifier.show_most_informative_features(50))
 
-# Webscrapes article text from a url
-html = requests.get(url_test).text
-text = newspaper.fulltext(html)
+    end = time.time()
+    print("Runtime:", end - start)
 
-custom_tokens = cleaning_article.run(text)
-print('The article is predicted as: ', classifier.classify(dict([token, True] for token in custom_tokens)))
+    # Pass in url of article to classify
+    url_test = input('\n\nCopy an Article URL Here Or Press q to Quit: ')
 
-dist = classifier.prob_classify(dict([token, True] for token in custom_tokens))
-list(dist.samples())
+    while url_test != "q":
+        # Webscrapes article text from a url
+        html = requests.get(url_test).text
+        text = newspaper.fulltext(html)
 
-left_prob = round(dist.prob('Left'), 4)
-right_prob = round(dist.prob('Right'), 4)
-print('Probability of being a left-leaning article:', left_prob)
-print('Probability of being a right-leaning article:', right_prob)
+        custom_tokens = cleaning_article.run(text)
+        print('The article is predicted as: ', classifier.classify(dict([token, True] for token in custom_tokens)))
 
-ex_score = round(max(left_prob, right_prob) - min(left_prob, right_prob), 4)
-print('Extremity score: ', ex_score)
+        dist = classifier.prob_classify(dict([token, True] for token in custom_tokens))
+        list(dist.samples())
+
+        left_prob = round(dist.prob('Left'), 4)
+        right_prob = round(dist.prob('Right'), 4)
+        print('Probability of being a left-leaning article:', left_prob)
+        print('Probability of being a right-leaning article:', right_prob)
+
+        ex_score = round(max(left_prob, right_prob) - min(left_prob, right_prob), 4)
+        print('Extremity score: ', ex_score)
+
+        url_test = input('\n\nCopy an Article URL Here Or Press q to Quit: ')
+
